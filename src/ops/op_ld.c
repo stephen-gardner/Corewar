19200ffe371f332746b52457b2131783317451af
Stephen Gardner <stephenbgardner@gmail.com>
2018-10-28 01:09:00 -0700

    Revert ld change

--- src/ops/op_ld.c
+++ src/ops/op_ld.c
@@ -6,7 +6,7 @@
 /*   By: sgardner <stephenbgardner@gmail.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/27 07:37:26 by sgardner          #+#    #+#             */
-/*   Updated: 2018/10/27 23:50:39 by sgardner         ###   ########.fr       */
+/*   Updated: 2018/10/27 08:38:26 by sgardner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,9 @@ t_bool	op_ld(t_core *core, t_proc *p)
 {
 	t_instr	*instr;
 	t_uint	*dst;
-	t_byte	*src;
 
 	instr = &p->instr;
-	src = IDX_POS(core->arena, p->pc, read_data(core, instr, 0));
 	dst = (t_uint *)instr->args[1];
-	*dst = read_core(core, src, REG_SIZE);
+	*dst = read_data(core, instr, 0);
 	return (!*dst);
 }
